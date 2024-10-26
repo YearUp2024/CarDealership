b10b65d48bf27fdd5921e93bfc128a99b0825f69
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-26 17:15:34 -0400

    Adding methods to UserInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,32 @@
 package com.pluralsight;
 
 public class UserInterface {
+    private Dealership dealership;
+
+    public UserInterface(Dealership dealership){
+        this.dealership = dealership;
+    }
+
+    public void display(){
+
+    }
+
+    public void processGetByPriceRequest(){}
+
+    public void processGetByMakeModelRequest(){}
+
+    public void processGetByYearRequest(){}
+
+    public void processGetByColorKequest(){}
+
+    public void processGetByMileageRequest(){}
+
+    public void processGetByVehicleTypeRequest(){}
+
+    public void processGetAllVehicleaRequest(){}
+
+    public void processAddVehicleRequest(){}
+
+    public void rocessRemoveVehicleRequest(){}
 
 }
